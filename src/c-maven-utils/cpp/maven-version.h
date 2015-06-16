d8b0cd90b564d59f1c017e39e67b0f47b000370b
Nate Rosenblum <flander@gmail.com>
2015-06-15 21:20:04 -0700

    Use shared_ptr for copyable cpp bindings

--- src/c-maven-utils/cpp/maven-version.h
+++ src/c-maven-utils/cpp/maven-version.h
@@ -21,6 +21,7 @@
 #ifndef CPP_MAVEN_VERSION_H_
 #define CPP_MAVEN_VERSION_H_
 
+#include <memory>
 #include <string>
 
 #include "c-maven-utils/maven-version.h"
@@ -30,26 +31,27 @@ namespace mvn {
 class Version {
 public:
     explicit Version(std::string const& version);
-    ~Version();
     bool operator<(Version const& o) const;
     bool operator==(Version const& o) const;
+
+    struct Deleter {
+        void operator()(struct maven_version *v) const {
+            mv_free(v);
+        }
+    };
 private:
-    struct maven_version *version_;
+    std::shared_ptr<struct maven_version> version_;
 };
 
 Version::Version(std::string const& version)
-    : version_(mv_parse(version.c_str())) { }
-
-Version::~Version() {
-    mv_free(version_);
-}
+    : version_(mv_parse(version.c_str()), Deleter()) { }
 
 bool Version::operator<(Version const& o) const {
-    return mv_compare(version_, o.version_) < 0;
+    return mv_compare(version_.get(), o.version_.get()) < 0;
 }
 
 bool Version::operator==(Version const& o) const {
-    return mv_compare(version_, o.version_) == 0;
+    return mv_compare(version_.get(), o.version_.get()) == 0;
 }
 
 } // mvn namespace
